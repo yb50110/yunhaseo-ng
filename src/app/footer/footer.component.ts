16e65cdf9d9c1d1b9c32f3782fb8a71e41716fc9
yunhaseo <yunha@b302.nl>
2018-02-18 12:50:00 -0600

    Updated footer links so that it is visible in mobile

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,7 +4,7 @@ import {Component, OnInit} from '@angular/core';
     selector: 'app-footer',
     template: `
         <footer>
-            <p><a href="https://github.com/yb50110" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/yunha-seo/" target="_blank">LinkedIn</a></p>
+            <p class="footer-links"><a href="https://github.com/yb50110" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/yunha-seo/" target="_blank">LinkedIn</a></p>
             <p>Copyright © 2017 of Yun Ha Seo</p>
         </footer>
     `,
